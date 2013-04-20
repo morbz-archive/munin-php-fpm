f86f4443c2266a316d922d00c9b6c86180927089
MorbZ <morbz1@gmail.com>
2013-04-20 22:52:55 +0200

    Full revision

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
+This is a revised version of [Devlopnet / munin-php-fpm](https://github.com/Devlopnet/munin-php-fpm) with the difference that processes are combined based on their pools. Also there is an average process age graph and a process count graph.
+
 Setup PHP-FPM
 -------------
 
 This plugin requires PHP CLI.
 
-- ln -s /usr/share/munin/plugins/php-fpm /etc/munin/plugins/php-fpm-status : Provides graph about php-fpm processes status  
-- ln -s /usr/share/munin/plugins/php-fpm /etc/munin/plugins/php-fpm-memory : Provides graph about Memory processes usage  
-- ln -s /usr/share/munin/plugins/php-fpm /etc/munin/plugins/php-fpm-memoryPreview : Provides graph about Memory processes pools usage (RAM max, RAM min and RAM average)  
-- ln -s /usr/share/munin/plugins/php-fpm /etc/munin/plugins/php-fpm-cpu : Provides graph about processes CPU usage  
+### Install Plugin
+`$ cd /usr/share/munin/plugins/`  
+`$ [sudo] wget https://github.com/MorbZ/munin-php-fpm/blob/master/php-fpm.php`  
+`$ [sudo] chmod +x php-fpm.php`
 
-More information
-----------------
+### Setup Graphs
+Average process memory per pool:  
+`$ [sudo] ln -s /usr/share/munin/plugins/php-fpm /etc/munin/plugins/php-fpm-memory`
 
-See https://github.com/sosedoff/munin-plugins/tree/master/php-fpm for installation informations and to see another php-fpm plugin.
+CPU per pool:  
+`$ [sudo] ln -s /usr/share/munin/plugins/php-fpm /etc/munin/plugins/php-fpm-cpu`
+
+Number of processes per pool:  
+`$ [sudo] ln -s /usr/share/munin/plugins/php-fpm /etc/munin/plugins/php-fpm-count`
+
+Average process age per pool:  
+`$ [sudo] ln -s /usr/share/munin/plugins/php-fpm /etc/munin/plugins/php-fpm-time`
+
+Dont' forget to restart Munin after changing plugins:  
+`$ [sudo] service munin-node restart`
--- php-fpm.php
+++ php-fpm.php
@@ -4,144 +4,74 @@
 
 /**
 * @author Devlopnet
+* @author MorbZ
 * @licence CC
 */
 
-exec("ps aux | grep php-fpm | awk '{print $11,$3,$6,$8,$12,$13}'", $result);
+exec("ps -eo %cpu,etime,rss,command | grep php-fpm", $result);
 
-$states = array(
-    'D'    =>    'Uninterruptible sleep',
-    'R'    =>    'Running or runnable',
-    'S'    =>    'Interruptible sleep',
-    'T'    =>    'Stopped',
-    'W'    =>    'Paging',
-    'X'    =>    'Dead',
-    'Z'    =>    'Defund (Zombie)'
-);
-
+//iterate through processes
 $groups = array();
-
-foreach ($result as $line)
-{
-    $args = explode(' ', $line);
-    if (strpos(array_shift($args), 'php-fpm') === false)
-    {
+foreach ($result as $line) {
+	//split fields
+	$line = trim($line);
+	$args = preg_split('/\s+/', $line);
+    if (strpos($args[3], 'php-fpm') === false) {
         continue;
     }
-    list($cpu, $ram, $state, $type, $poolName) = $args;
-    
-    $state = substr($state, 0, 1);
+    list($cpu, $time, $ram, $type, $poolWord, $poolName) = $args;
 
-    if ($type == 'master') $groupName = 'Master';
-    else               $groupName = $poolName;
+	//which group
+    if ($poolWord == 'master') {
+    	$groupName = 'master';
+    } else {
+    	$groupName = $poolName;
+    }
 
-    if (!isset($groups[$groupName]))
-    {
-        $groups[$groupName] = array();
-        foreach ($states as $key=>$currState)
-        {
-            $groups[$groupName]['status'][$key] = 0;
-        }
+	//add group
+    if (!isset($groups[$groupName])) {
+        $groups[$groupName] = array(
+        	'count' => 0,
+        	'memory' => 0,
+        	'cpu' => 0,
+        	'time' => 0
+        );
     }
 	
-    $groups[$groupName]['status'][$state]++;
-	$ramMo = $ram / 1024;
-	$groups[$groupName]['memory'][] = $ramMo;
-	if ($type != 'master')
-	{
-		if (!isset($groups[$groupName]['minMemory'])) {
-			$groups[$groupName]['maxMemory'] = 0;
-			$groups[$groupName]['minMemory'] = PHP_INT_MAX;
-		}
-		if ($ramMo > $groups[$groupName]['maxMemory']) {
-			$groups[$groupName]['maxMemory'] = $ramMo;
-		}
-		if ($ramMo < $groups[$groupName]['minMemory']) {
-			$groups[$groupName]['minMemory'] = $ramMo;
-		}
-		$groups[$groupName]['averageMemory'] = array_sum($groups[$groupName]['memory']) / count($groups[$groupName]['memory']);
-	}
-    $groups[$groupName]['cpu'][] = $cpu;
+	//add values
+	$groups[$groupName]['count']++;
+	$groups[$groupName]['cpu'] += $cpu;
+	$groups[$groupName]['time'] += timeToSeconds($time);
+	$groups[$groupName]['memory'] += $ram / 1024;
 }
 
+//check args
+if(!isset($argv) || !isset($argv[0])) {
+	die("Error: No Plugin name provided\n");
+}
 $fileCalled = basename($argv[0]);
 $isConfig = isset($argv[1]) && $argv[1] == 'config';
 
-switch ($fileCalled)
-{
-// ------------------------------------------------------
-	case 'php-fpm-status':
-// ------------------------------------------------------	
-		$elements = array();
-		foreach ($groups as $name=>$array)
-		{
-			foreach ($array['status'] as $state=>$number)
-			{
-				$id = $name.'_'.$state;
-				$elements[$id] = array(
-					'label'	=>	$name . ' : ' . $states[$state],
-					'type'	=>	'GAUGE',
-					'value'	=>	$number
-				);		
-			}
-		}
-		$config = array(
-			'title'		=>	'PHP-FPM Status',
-			'legend'	=>	'Nombre processus',
-			'elements'	=>	$elements
-		);
-		break;
+//which plugin?
+switch ($fileCalled) {
 // ------------------------------------------------------		
 	case 'php-fpm-memory':
 // ------------------------------------------------------
 		$elements = array();
-		foreach ($groups as $name=>$array)
-		{
-			foreach ($array['memory'] as $nbProc=>$memProcessus)
-			{
-				$id = $name.'_'.$nbProc;
-				$elements[$id] = array(
-					'label'	=>	$name . ' : Processus n°' . $nbProc,
-					'type'	=>	'GAUGE',
-					'value'	=>	$memProcessus
-				);
-			}
+		foreach ($groups as $name=>$array) {
+			$ramMb = $array['memory'] / $array['count'];
+			$label = $name == 'master' ? 'Master' : 'Pool ' . $name;
+			$elements[$name] = array(
+				'label'	=>	$label,
+				'type'	=>	'GAUGE',
+				'value'	=>	$ramMb
+			);
 		}
 		$config = array(
-			'title'		=>	'PHP-FPM Memory',
-			'legend'	=>	'Mémoire Mo',
-			'elements'	=>	$elements
-		);	
-		break;
-// ------------------------------------------------------		
-	case 'php-fpm-memoryPreview':
-// ------------------------------------------------------
-		foreach ($groups as $name=>$array)
-		{
-			if (isset($array['averageMemory']))
-			{
-				$elements = array(
-					$name.'_'.'minMemory' => array(
-						'label'	=>	$name . ' : Min',
-						'type'	=>	'GAUGE',
-						'value'	=>	$array['minMemory']
-					),
-					$name.'_'.'averageMemory' => array(
-						'label'	=>	$name . ' : Moyenne',
-						'type'	=>	'GAUGE',
-						'value'	=>	$array['averageMemory']
-					),
-					$name.'_'.'maxMemory' => array(
-						'label'	=>	$name . ' : Max',
-						'type'	=>	'GAUGE',
-						'value'	=>	$array['maxMemory']
-					)
-				);				
-			}
-		}
-		$config = array(
-			'title'		=>	'PHP-FPM Memory Preview',
-			'legend'	=>	'Mémoire Mo',
+			'params' => array(
+				'graph_title' => 'PHP-FPM Average Process Memory',
+				'graph_vlabel' => 'MB'
+			),
 			'elements'	=>	$elements
 		);	
 		break;
@@ -149,45 +79,115 @@ switch ($fileCalled)
 	case 'php-fpm-cpu':
 // ------------------------------------------------------
 		$elements = array();
-		foreach ($groups as $name=>$array)
-		{
-			foreach ($array['cpu'] as $nbProc=>$memProcessus)
-			{
-				$id = $name.'_'.$nbProc;
-				$elements[$id] = array(
-					'label'	=>	$name . ' : Processus n°' . $nbProc,
-					'type'	=>	'GAUGE',
-					'value'	=>	$memProcessus
-				);		
-			}
+		foreach ($groups as $name=>$array) {
+			$cpu = $array['cpu'];
+			$label = $name == 'master' ? 'Master' : 'Pool ' . $name;
+			$elements[$name] = array(
+				'label'	=>	$label,
+				'type'	=>	'GAUGE',
+				'value'	=>	$cpu
+			);
 		}
 		$config = array(
-			'title'		=>	'PHP-FPM CPU',
-			'legend'	=>	'%',
+			'params' => array(
+				'graph_title' => 'PHP-FPM CPU',
+				'graph_vlabel' => '%',
+				'graph_scale' => 'no'
+			),
+			'elements'	=>	$elements
+		);	
+		break;
+// ------------------------------------------------------		
+	case 'php-fpm-count':
+// ------------------------------------------------------
+		$elements = array();
+		foreach ($groups as $name=>$array) {
+			if($name == 'master') {
+				continue;
+			}
+			$elements[$name] = array(
+				'label'	=>	'Pool ' . $name,
+				'type'	=>	'GAUGE',
+				'value'	=>	$array['count']
+			);
+		}
+		$config = array(
+			'params' => array(
+				'graph_title' => 'PHP-FPM Processes',
+				'graph_vlabel' => 'processes'
+			),
+			'elements'	=>	$elements
+		);	
+		break;
+// ------------------------------------------------------		
+	case 'php-fpm-time':
+// ------------------------------------------------------
+		$elements = array();
+		foreach ($groups as $name=>$array) {
+			if($name == 'master') {
+				continue;
+			}
+			$time = round($array['time'] / $array['count']);
+			$elements[$name] = array(
+				'label'	=>	'Pool ' . $name,
+				'type'	=>	'GAUGE',
+				'value'	=>	$time
+			);
+		}
+		$config = array(
+			'params' => array(
+				'graph_title' => 'PHP-FPM Average Process Age',
+				'graph_vlabel' => 'seconds',
+				'graph_scale' => 'no'
+			),
 			'elements'	=>	$elements
 		);	
 		break;
 // ------------------------------------------------------
 	default:
-		die("Unrecognized Plugin name $fileCalled\n");
+		die("Error: Unrecognized Plugin name $fileCalled\n");
 }
 
-if ($isConfig)
-{
-	echo 'graph_title ' . $config['title'] . "\n";
-	echo 'graph_vlabel ' . $config['legend'] . "\n";
+//output
+if ($isConfig) {
+	//graph params
 	echo "graph_category PHP-FPM\n";
-	foreach ($config['elements'] as $element=>$data)
-	{
-		foreach ($data as $key=>$value)
-		{
+	foreach($config['params'] as $key=>$value) {
+		echo $key . ' ' . $value . "\n";
+	}
+	
+	//element params
+	foreach($config['elements'] as $element=>$data) {
+		foreach ($data as $key=>$value) {
 			if ($key == 'value') continue;
 			echo $element . '.' . $key . ' ' . $value . "\n";
 		}
 	}
 } else {
-	foreach ($config['elements'] as $element=>$data)
-	{
-		echo $element . '.value ' . $data['value'] . "\n";
+	//element values
+	foreach ($config['elements'] as $pool=>$element) {
+		echo $pool . '.value ' . $element['value'] . "\n";
 	}
+}
+
+//functions
+function timeToSeconds ($time) {
+	$seconds = 0;
+	
+	//days
+	$parts = explode('-', $time);
+	if(count($parts) == 2) {
+		$seconds += $parts[0] * 86400;
+		$time = $parts[1];
+	}
+	
+	//hours
+	$parts = explode(':', $time);
+	if(count($parts) == 3) {
+		$seconds += array_shift($parts) * 3600;
+	}
+	
+	//minutes/seconds
+	$seconds += $parts[0] * 60 + $parts[1];
+	return $seconds;
 }
